6116dd05f8fec54048536307d0ca529368b4aa7e
Ashiqur Rahman Zeeshan <zeeshan.nvmx@gmail.com>
2024-07-02 02:27:19 +0600

    statuscode change

--- controllers/authController.js
+++ controllers/authController.js
@@ -24,7 +24,7 @@ const login = async (req, res) => {
     const { username, password, location } = req.body
     const user = await User.findOne({ username })
     if (!user || !(await user.comparePassword(password))) {
-      return res.status(401).json({ message: 'Invalid credentials' })
+      return res.status(400).json({ message: 'Invalid credentials' })
     }
     const token = jwt.sign({ id: user._id, username: user.username, role: user.role }, process.env.JWT_SECRET, {
       expiresIn: process.env.JWT_EXPIRATION_MINUTES * 60 || 60 * 24 * 7 * 60,
@@ -51,7 +51,7 @@ const showme = async (req, res) => {
   try {
     return res.status(200).json({ id: req.user._id, username: req.user.username, role: req.user.role })
   } catch (error) {
-    return res.status(401).json({ message: 'Invalid token' })
+    return res.status(400).json({ message: 'Invalid token' })
   }
 }
 
